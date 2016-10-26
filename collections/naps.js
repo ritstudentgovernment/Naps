4707bff0cb99c903b1f048e8d6cc2a3108e514fd
Omar De La Hoz <omar.dlhz@hotmail.com>
2016-10-26 15:31:15 -0400

    Resized images to 300x300

--- collections/naps.js
+++ collections/naps.js
@@ -74,7 +74,8 @@ Naps.allow({
 NapsFS = new FS.Collection('napsFS', {
 	stores: [new FS.Store.FileSystem('napsFS', {
 		transformWrite: function(fileObj, readStream, writeStream) {
-			gm(readStream, fileObj.name).autoOrient().stream().pipe(writeStream);
+			// Depends on GraphicsMagick.
+			gm(readStream, fileObj.name).resize(300, 300).autoOrient().stream().pipe(writeStream);
 		}
 	})]
 });
