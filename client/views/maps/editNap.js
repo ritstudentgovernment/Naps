a91bb3dcb1a971beab513069f85e96cf4e5210f1
Omar De La Hoz <omar.dlhz@hotmail.com>
2016-11-01 17:35:31 -0400

    Modified getLocation to work with mobile

--- client/views/maps/editNap.js
+++ client/views/maps/editNap.js
@@ -1,16 +1,56 @@
+////////////////////////////////////////////////////////////////////////////////
+///                   editNap.js
+///
+///Author   : Omar De La Hoz (oed7416@rit.edu)
+///Description  : Javascript file for editNap template.
+///Date Created : 09/27/16
+///updated      : 11/01/16
+////////////////////////////////////////////////////////////////////////////////
+
 Template.editNap.events({
   'click #geoLoc' : function(e){
     e.preventDefault();
-    Location.locate(function(pos){
-       $('input[name=lat]').val(pos.latitude);
-       $('input[name=lng]').val(pos.longitude);
-
-       var napId = Session.get('editingNap')._id;
-
-       markers[napId].setPosition({ lat: pos.latitude, lng: pos.longitude });
-
-    }, function(err){
-       console.log("Oops! There was an error", err);
-    });
+    window.navigator.geolocation.getCurrentPosition(locationSuccess, locationError);
   }
 });
+
+
+/**
+ * Sets the current location coordinates to the fields
+ * and adds marker to map.
+ * @param  position Users current position.
+ */
+function locationSuccess(position){
+
+  var coordinates = position.coords;
+  $('input[name=lat]').val(coordinates.latitude);
+  $('input[name=lng]').val(coordinates.longitude);
+
+  if(previewMarker[0]){
+    previewMarker[0].setPosition({ lat: coordinates.latitude, lng: coordinates.longitude });
+  }
+  else{
+
+    var marker = new google.maps.Marker({
+          draggable: true,
+          animation: google.maps.Animation.DROP,
+          position: { lat: coordinates.latitude, lng: coordinates.longitude },
+          icon: Session.get("previewImage"),
+          map: GoogleMaps.maps.napMap.instance
+    });
+
+    google.maps.event.addListener(marker, 'dragend');
+    previewMarker[0] = marker;
+  }
+}
+
+
+/**
+ * Prints the error to console if something went
+ * wrong when getting the users location.
+ * @param  err  Error message.
+ */
+function locationError(err){
+
+  console.log("Oops! There was an error", err);
+}
